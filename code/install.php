78656e54661b8fce0188d9b9db47184ebe291a00
Dario <darios000@gmail.com>
2020-05-28 20:47:09 +0200

    updated install page

--- code/install.php
+++ code/install.php
@@ -14,10 +14,12 @@ $user_name = $pass = $file_contents = "";
 <body>
     <div class="container">
         <h1 class="title">What's the weather?</h1>
-        <p class="subtitle">Enter the name of a city.</p>
+        <p class="subtitle">Setup data.</p>
 
         <form action="<?= htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post" id="form">
             <div class="flex-col">
+                <label for="server" class="invisible"><small>server address</small></label>
+                <input type="text" name="server" id="server" placeholder="DB server address">
                 <label for="username" class="invisible"><small>database user name</small></label><br>
                 <input name="username" id="username" placeholder="DB user name">
                 <label for="password" class="invisible"><small>database password</small></label>
@@ -32,10 +34,11 @@ $user_name = $pass = $file_contents = "";
         <div class="response response-ok ow">
             <?php
             if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                $host = trim($_POST["server"]);
                 $user_name = trim($_POST["username"]);
                 $pass = trim($_POST["password"]);
                 $api_key = trim($_POST["api-key"]);
-                $file_contents = "[Credentials]\nuser = {$user_name}\npass = {$pass}\nhost = localhost\ndatabase = city_codes\napi-key = {$api_key}";
+                $file_contents = "[Credentials]\nuser = {$user_name}\npass = {$pass}\nhost = {$host}\ndatabase = city_codes\napi-key = {$api_key}";
                 file_put_contents("script/dbc.ini", $file_contents);
                 echo "Creating new database...<br>";
                 include("script/json_to_db.php");
